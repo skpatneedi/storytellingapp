3c689390e4a812c702681f311b39a5f57fb5f71d
skpatneedi <71281744+skpatneedi@users.noreply.github.com>
2021-08-08 17:44:17 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCw2eZAftGd-y5Hz0e_s7RkHCdQTvmD2Rw",
+    authDomain: "storytellingapp-724cd.firebaseapp.com",
+    projectId: "storytellingapp-724cd",
+    storageBucket: "storytellingapp-724cd.appspot.com",
+    messagingSenderId: "640234555229",
+    appId: "1:640234555229:web:4f41f9ff5f7e44a719cab4"
+  };
+ // if (!firebaseConfig.apps.length) {
+   
+ // } else {
+   // firebase.app();
+  //}
+  
